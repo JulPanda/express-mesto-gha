da8609e07beef288b87b44c022b41fba9ec59306
Julia Popova <popova.juli@gmail.com>
2023-07-22 18:38:41 +0300

    Проект 14.V2.Test1

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { ERROR_NOT_FOUND } = require('./utils/constants');
+const NotFoundError = require('./errors/notFoundError');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
-app.use('*', (req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый адрес не существует' });
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый адрес не существует'));
 });
 
 app.use(errors());
